ee071c8ab8a1d221da668d1dc2025a43b1648ed9
rawatchetan2026-max <rawatchetan2026@gmail.com>
2026-03-25 21:28:04 +0530

    learning java Lang package

---
+++ _Java_lang_Package/langdemo.java
@@ -0,0 +1,24 @@
+public class langdemo {
+    public static void main(String[] args) {
+        Myobj o = new Myobj();
+        Myobj o1 = new Myobj();
+        System.out.println(o.equals(o1));
+        System.out.println(o.toString());
+        System.out.println(o.hashCode());
+
+    }
+}
+
+class Myobj {
+    public String toString(){
+        return "My object";
+    }
+ 
+    public int hashCode() {
+        return 100;
+    }
+
+    public boolean equals(Object o) {
+        return this.hashCode() == o.hashCode();
+    }
+}
