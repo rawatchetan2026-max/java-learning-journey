b02d0aa43ff76b15db4fbfa572f1131ea776df4a
rawatchetan2026-max <rawatchetan2026@gmail.com>
2026-03-28 20:35:49 +0530

    Create Mathdemo.java

---
+++ _Java_lang_Package/Mathdemo.java
@@ -0,0 +1,21 @@
+public class Mathdemo {
+    public static void main(String[] args) {
+        System.out.print("Absolute : ");
+        System.out.println(Math.abs(-123));
+
+        System.out.print("Absolute : ");
+        System.out.println(StrictMath.abs(-123));
+
+        System.out.print("Cube root");
+        System.out.println(Math.cbrt(0));
+
+        System.out.print("Exact Decrement : ");
+        System.out.println(Math.decrementExact(0));
+
+        // System.out.print("");
+        // System.out.println(Math.cbrt(0));
+
+        // System.out.print("Cube root");
+        // System.out.println(Math.cbrt(0));
+    }
+}
