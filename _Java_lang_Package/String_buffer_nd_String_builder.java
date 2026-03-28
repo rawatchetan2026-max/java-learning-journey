071fec5446ba27123c05adf8307807ea7b4953bc
rawatchetan2026-max <rawatchetan2026@gmail.com>
2026-03-28 20:35:58 +0530

    Update String_buffer_nd_String_builder.java

---
+++ _Java_lang_Package/String_buffer_nd_String_builder.java
@@ -0,0 +1,15 @@
+public class String_buffer_nd_String_builder {
+    public static void main(String[] args) {
+        String s1 = new String("hello");
+        StringBuffer s2 = new StringBuffer("hello");
+        StringBuilder s3 = new StringBuilder("hello");
+
+        s1.concat(" world");
+        s2.append(" world");
+        s3.append(" world");
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+    }
+}
