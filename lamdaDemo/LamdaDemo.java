1a4a8ef60b09917c97cd3dbdabb3a05294c5dc2b
rawatchetan2026-max <rawatchetan2026@gmail.com>
2026-03-29 20:42:51 +0530

    Update LamdaDemo.java

--- lamdaDemo/LamdaDemo.java
+++ lamdaDemo/LamdaDemo.java
@@ -1,15 +1,30 @@
+// public class LamdaDemo {
+
+//     @FunctionalInterface
+//     interface MyInterface {
+//         public void show();
+//     }
+
+//     public static void main(String[] args) {
+// anonymous class 
+//         MyInterface obj = new MyInterface() {
+//             public void show() {
+//                 System.out.println("hello world");
+//             }
+//         };
+//         obj.show();
+//     }
+// }
 public class LamdaDemo {
-    @FunctionalInterface
     interface MyInterface {
         public void show();
     }
 
     public static void main(String[] args) {
-        MyInterface obj = new MyInterface() {
-            public void show() {
-                System.out.println("hello world");
-            }
+        MyInterface obj = () -> {
+            System.out.println("hello world");
         };
         obj.show();
     }
+
 }
