3a414e5cbed1de4fbb6fb1105a1ca458eb55edc8
rawatchetan2026-max <rawatchetan2026@gmail.com>
2026-03-31 23:07:38 +0530

    java io

--- Output_Stream.java
+++ Output_Stream.java
@@ -1,13 +1,15 @@
 import java.io.*;
 
-public class FileExample {
+public class Output_Stream {
     public static void main(String[] args) {
         try {
             FileOutputStream fos = new FileOutputStream(
                     "Output.txt");
 
-            String str = "learning Java Prorgamming";
+            String str = "learning Java Prorgamming \n";
             fos.write(str.getBytes());
+            String str1 = "learning java programming day 8";
+            fos.write(str1.getBytes(), 9, str1.length() - 9);
             fos.close();
 
         } catch (FileNotFoundException e) {
