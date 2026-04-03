450585638dd8fe8604035e3739b04d63b915081e
rawatchetan2026-max <rawatchetan2026@gmail.com>
2026-04-03 21:13:50 +0530

    Create FileHandling.java

---
+++ Input_Output_Stream/FileHandling.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class FileHandling {
+    public static void main(String[] args) throws Exception {
+
+        File f = new File(
+                "C:\\Users\\himmat\\Documents\\Desktop\\Downloads\\Favorites\\Documents\\GitHub\\java-learning-journey\\lamdaDemo");
+        System.out.println(f.getCanonicalPath());
+
+        System.out.println(f.isDirectory());
+        // System.out.println(Arrays.toString(f.list()));
+
+        File list[] = f.listFiles();
+
+        for (File x : list) {
+            System.out.println("name : " + x.getName() + "   ");
+            System.out.print("path : " + x.getPath() + " ");
+            System.out.print("parent : " + x.getParent());
+        }
+
+    }
+}
