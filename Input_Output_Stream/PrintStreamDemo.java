260f9bfb78cdbcf6f90a69fe7c8cd9fadbf3dd8c
rawatchetan2026-max <rawatchetan2026@gmail.com>
2026-04-04 22:14:46 +0530

    Create PrintStreamDemo.java

---
+++ Input_Output_Stream/PrintStreamDemo.java
@@ -0,0 +1,58 @@
+
+import java.io.*; //importing java input output to the program
+
+class Student { // created a class having members : roll , Name , Dept of diff. data types
+    int roll;
+    String Name;
+    String Dept;
+}
+
+class MyRead {
+    public void read() throws Exception {
+        // instead of using try-catch blocks which makes code lengthy
+        // so, we used throws keyword for all type of the Exceptions
+
+        FileInputStream fis = new FileInputStream("My.txt"); // File input stream is use to read the file in bytes...
+                                                             // also it cannot read Strings
+        BufferedReader br = new BufferedReader(new InputStreamReader(fis)); // Buffered reader to read the file in one
+                                                                            // go in Character form also buffered reader
+                                                                            // is having a method called readLine() or
+                                                                            // read as a String
+
+        Student s = new Student(); // obj of Student class
+
+        // giving the values to the members of the Student class from the My.txt
+
+        s.roll = Integer.parseInt(br.readLine());
+        s.Name = br.readLine();
+        s.Dept = br.readLine();
+
+        System.out.println("rollno : " + s.roll + " Name : " + s.Name + " Department : " + s.Dept);
+
+        br.close();
+    }
+}
+
+public class PrintStreamDemo {
+
+    public static void main(String[] args) throws Exception {
+        FileOutputStream fo = new FileOutputStream("My.txt"); // It is used to write the file if file not present then
+                                                              // it creates and then edit it
+        PrintStream ps = new PrintStream(fo); // it is used to print the members if an members of class to a file..
+
+        Student s = new Student();
+        s.roll = 10;
+        s.Name = "Chetan Rawat";
+        s.Dept = "CSE";
+
+        ps.println(s.roll);
+        ps.println(s.Name);
+        ps.println(s.Dept);
+
+        ps.close();
+
+        new MyRead().read();
+
+    }
+
+}
