ee3cd02691449acdff106220ded09b2f16bba8e4
rawatchetan2026-max <rawatchetan2026@gmail.com>
2026-04-09 20:53:07 +0530

    Update GenericCode.java

--- GenericDemo/GenericCode.java
+++ GenericDemo/GenericCode.java
@@ -7,7 +7,7 @@ public class GenericCode<T> {
     T data[] = (T[]) new Object[3]; // array of Object type cast as genric
 
     public static void main(String[] args) {
-        // Object obj[] = new Object[3];
+        // Object obj[] = new Object[3]; array of Object is type-safe
         // obj[0] = "hi";
         // obj[1] = "hi";
         // obj[2] = new Integer(10);
@@ -19,6 +19,5 @@ public class GenericCode<T> {
         gc.data[1] = "bye";
         // gc.data[2] = 10; //can't be inserted to the array coz now the comiler knows::
         // T belongs to String
-
     }
 }
