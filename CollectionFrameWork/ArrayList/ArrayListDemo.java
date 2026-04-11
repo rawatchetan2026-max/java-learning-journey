dc79f4a842e3df9fc5f4877c580b778559e74ed0
rawatchetan2026-max <rawatchetan2026@gmail.com>
2026-04-11 22:15:37 +0530

    Create ArrayListDemo.java

---
+++ CollectionFrameWork/ArrayList/ArrayListDemo.java
@@ -0,0 +1,29 @@
+package CollectionFrameWork;
+
+import java.util.*;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        ArrayList<Integer> al1 = new ArrayList<>();
+        al1.add(10);
+        al1.add(60);
+        al1.add(70);
+
+        ArrayList<Integer> al2 = new ArrayList<>(Arrays.asList(20, 30, 40, 50));
+
+        al1.addAll(1, al2);
+        al1.add(5, 70);
+        System.out.println(al1);
+        System.out.println(al1.indexOf(70));
+        System.out.println(al1.lastIndexOf(70));
+        System.out.println(al2);
+        System.out.println(al1.size());
+
+        Iterator<Integer> it = al1.iterator();
+
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+
+    }
+}
