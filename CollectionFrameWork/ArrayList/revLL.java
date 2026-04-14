e21e4a42417d29f9e09e569c15eb645fba810806
rawatchetan2026-max <rawatchetan2026@gmail.com>
2026-04-14 21:26:49 +0530

    Create revLL.java

---
+++ CollectionFrameWork/ArrayList/revLL.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class revLL {
+    public static void main(String[] args) {
+        LinkedList<Integer> L = new LinkedList<>(Arrays.asList(10, 20, 30, 40, 50));
+        System.out.println(L);
+
+    }
+}
