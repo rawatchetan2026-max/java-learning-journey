52147f27892fac25a7d06f16f86460e5156469f4
rawatchetan2026-max <rawatchetan2026@gmail.com>
2026-04-18 23:11:22 +0530

    Create ComparableDemo.java

---
+++ CollectionFrameWork/List/ComparableDemo.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Point implements Comparable {
+    int x, y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public String toString() {
+        return "(" + x + " , " + y + ")";
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Point p = (Point) o;
+        if (this.x < p.x) {
+            return -1;
+        } else if (this.x > p.x) {
+            return 1;
+        } else {
+            if (this.y > p.y) {
+                return 1;
+            } else if (this.y < p.y) {
+                return -1;
+            }
+        }
+        return 0;
+    }
+}
+
+public class ComparableDemo {
+
+    public static void main(String[] args) {
+        TreeSet<Point> ts = new TreeSet<>();
+
+        ts.add(new Point(1, 1));
+        ts.add(new Point(3, 2));
+        ts.add(new Point(5, 4));
+
+        System.out.println(ts);
+    }
+}
